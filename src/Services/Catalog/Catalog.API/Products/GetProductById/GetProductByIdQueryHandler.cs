42a2fbc2e74bb890ca993575a451763b8fa53b09
Marija Topalova <marijatopalova1@outlook.com>
2024-10-14 01:27:48 +0200

    getProductById endpont and handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+    public record GetProductByIdResult(Product Product);
+
+    public class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+            var result = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            if (result is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByIdResult(result);
+        }
+    }
+}
