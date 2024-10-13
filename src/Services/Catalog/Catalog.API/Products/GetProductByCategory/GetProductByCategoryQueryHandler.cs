d8ed735adf8fcb6082dfbdd9341981c864638341
Marija Topalova <marijatopalova1@outlook.com>
2024-10-14 01:42:00 +0200

    getProductByCategory endpoint and handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Products.GetProductById;
+using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+    public class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query);
+
+            var result = await session.Query<Product>()
+                .Where(p => p.Categories.Contains(query.Category))
+                .ToListAsync();
+
+            return new GetProductByCategoryResult(result);
+        }
+    }
+}
