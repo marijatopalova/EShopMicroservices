42b090c4c6d2ed7172ec239f4a54b7d82acf7c44
Marija Topalova <marijatopalova1@outlook.com>
2024-10-14 01:59:38 +0200

    updateProduct handler and endpont

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductCommand(Guid Id, Product Product) : ICommand<UpdateProductResult>;
+
+    public record UpdateProductResult(bool IsSuccess);
+
+    public class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+        : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            product.Name = command.Product.Name;
+            product.Description = command.Product.Description;
+            product.Categories = command.Product.Categories;
+            product.ImageFile = command.Product.ImageFile;
+            product.Price = command.Product.Price;
+
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductResult(true);
+        }
+    }
+}
