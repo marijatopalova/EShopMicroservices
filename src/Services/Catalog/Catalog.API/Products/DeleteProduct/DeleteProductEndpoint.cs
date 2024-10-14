8a1d9fd5cd213d30a27e64efa1a2d9f2231c9d3a
Marija Topalova <marijatopalova1@outlook.com>
2024-10-14 02:21:28 +0200

    deleteProduct enpoint and handler

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,29 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    //public record DeleteProductRequest(Guid Id);
+
+    public record DeleteProductResponse(bool IsSuccess);
+
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id));
+
+                var resposne = result.Adapt<DeleteProductResponse>();
+
+                return Results.Ok(resposne);
+            })
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+        }
+    }
+}
