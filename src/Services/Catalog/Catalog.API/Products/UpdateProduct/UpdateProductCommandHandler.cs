f57c67a2b6c415338ab1173c14efe7bd0ea8f9fb
Marija Topalova <marijatopalova1@outlook.com>
2024-10-14 14:54:28 +0200

    FluentValidation for commands

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
@@ -1,9 +1,26 @@
 ﻿
+using Catalog.API.Products.CreateProduct;
+
 namespace Catalog.API.Products.UpdateProduct
 {
     public record UpdateProductCommand(Guid Id, Product Product) : ICommand<UpdateProductResult>;
 
     public record UpdateProductResult(bool IsSuccess);
+
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(x => x.Product.Id).NotEmpty().WithMessage("Id is required");
+
+            RuleFor(x => x.Product.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+
+            RuleFor(x => x.Product.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
 
     public class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
