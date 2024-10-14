4341354230d3fa2f55eece7c6701b5d8df9fe4fb
Marija Topalova <marijatopalova1@outlook.com>
2024-10-15 00:35:24 +0200

    StoreBasket and DeleteBasket command handlers and endpoints

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,6 +1,32 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿
+using Basket.API.Basket.StoreBasket;
+
+namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketEndpoint
+    public record DeleteBasketRequest(string UserName);
+
+    public record DeleteBasketResponse(bool IsSuccess);
+
+    public class DeleteBasketEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+            {
+                var command = new DeleteBasketCommand(userName);
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<DeleteBasketResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteBasket")
+            .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Basket")
+            .WithDescription("Delete Basket");
+        }
     }
 }
