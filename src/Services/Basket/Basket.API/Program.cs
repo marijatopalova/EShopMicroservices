19a5a3a2b06780d37a1389551f8bf51738c90c71
Marija Topalova <marijatopalova1@outlook.com>
2024-10-15 00:11:34 +0200

    Basket.API

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddCarter();
+
+var app = builder.Build();
+
+// Configure the http request pipeline
+
+app.Run();
